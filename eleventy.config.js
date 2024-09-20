a856594d2e8ed5fca56374f46f3871c40e0db345
Dan Webb <dan@etch.co>
2024-09-20 11:37:19 +0100

    feat: writing page

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,7 @@
 import browserslist from 'browserslist';
 import { transform, browserslistToTargets } from 'lightningcss';
 import webc from '@11ty/eleventy-plugin-webc';
+import renderSvg from './lib/renderSvg.js';
 
 async function transformCSS(content) {
   const targets = browserslistToTargets(browserslist('> 0.25% and not dead'));
@@ -17,6 +18,8 @@ async function transformCSS(content) {
 export default function(eleventyConfig) {
   // copy the assets folder to the output directory
   eleventyConfig.addPassthroughCopy('src/assets');
+
+  eleventyConfig.addJavaScriptFunction('renderSvg', renderSvg);
 
   eleventyConfig.addPlugin(webc, {
     components: 'src/components/**/*.webc',
